056480f2cf82cd55f42c52d0e1871ebd87ce149a
endigo9740 <gundamx9740@gmail.com>
2024-04-24 15:46:33 -0500

    Tailwind added

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config}*/
+const config = {
+	darkMode: 'selector',
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
+
+module.exports = config;
