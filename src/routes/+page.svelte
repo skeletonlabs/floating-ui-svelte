57c3d4e98e567cee2b50d19ef453cadae42b4a4b
abdel-17 <abdelrahmanb015@gmail.com>
2024-04-25 20:38:54 +0200

    update example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,35 @@
 <script lang="ts">
 	import { flip, offset } from '@floating-ui/dom';
-	import { useFloating } from '$lib/hooks/use-floating.svelte.js';
+	import { useFloating } from '$lib/hooks/use-floating/index.js';
+	import type { Placement } from '@floating-ui/dom';
 
-	const floating = useFloating({
-		placement: 'left',
-		middleware: [flip(), offset(2)]
+	const placement = $state<Placement>('left');
+	const elements = $state<{ reference?: HTMLElement; floating?: HTMLElement }>({});
+	const { floatingStyles } = useFloating({
+		get placement() {
+			return placement;
+		},
+		middleware: [flip(), offset(2)],
+		elements
 	});
 </script>
 
 <button
 	class="fixed left-4 top-4 rounded-md bg-green-300 p-2"
-	onclick={() => (floating.refs?.floating as HTMLElement | undefined)?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center'})}
+	onclick={() => {
+		elements.floating?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
+	}}
 >
 	Bring floating into view
 </button>
 
 <div class="flex h-[300vh] w-[300vw] items-center justify-center">
 	<div>
-		<button class="rounded-md bg-cyan-300 p-2" bind:this={floating.refs.reference}>Reference</button
-		>
+		<button class="rounded-md bg-cyan-300 p-2" bind:this={elements.reference}>Reference</button>
 		<div
 			class="absolute left-0 top-0 w-max rounded-md bg-red-300 p-4"
-			bind:this={floating.refs.floating}
-			style={floating.floatingStyles}
+			bind:this={elements.floating}
+			style={floatingStyles.value}
 		>
 			Floating
 		</div>
