73c792b20ada920f2eeec2cb3aac121932d390fb
endigo9740 <gundamx9740@gmail.com>
2024-04-25 16:36:25 -0500

    Basic page styles defined

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
 	import '../app.pcss';
+	import Navigation from '$lib/components/Navigation/Navigation.svelte';
 </script>
 
-<slot></slot>
+<div>
+	<!-- Navigation -->
+	<Navigation />
+	<!-- Main -->
+	<!-- NOTE: set px-20 for ToC -->
+	<main class="mx-72 py-10 pl-20">
+		<!-- Page Content -->
+		<slot />
+	</main>
+	<!-- Table of Contents -->
+	<!-- <div class="fixed top-0 right-0 bottom-0 z-0 w-72 p-4 overflow-y-auto no-scrollbar">(ToC)</div> -->
+</div>
---
+++ src/routes/temp/+page.svelte
@@ -0,0 +1,24 @@
+<div class="space-y-10">
+	<header class="card card-gradient space-y-8">
+		<h1 class="h1">useFloating</h1>
+		<p>
+			The main Hook of the library that acts as a controller for all other Hooks and components.
+		</p>
+		<pre class="pre">{`import { useFloating } from '@skeletonlabs/floatin-ui-svelte';`}</pre>
+	</header>
+	<section class="space-y-8">
+		<h2 class="h2">Usage</h2>
+		<p>Call the Hook inside a component.</p>
+		<pre class="pre">{`
+function App() {
+  const {refs, floatingStyles} = useFloating();
+  return (
+    <>
+      <div ref={refs.setReference} />
+      <div ref={refs.setFloating} style={floatingStyles} />
+    </>
+  );
+}
+        `.trim()}</pre>
+	</section>
+</div>
