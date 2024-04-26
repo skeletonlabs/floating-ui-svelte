d2c09bab0eb5b53165ba67d9dfc3eadbb8e88271
Chris Simmons <gundamx9740@gmail.com>
2024-04-26 16:45:07 -0500

    Website Design (#15)
    * Basic page styles defined
    
    * Layout and styles refined
    
    * Add Lucide, refinement
    
    * Update content and links
    
    * Scrollbars
    
    * Page title
    
    * Drawer added, but broken
    
    * Mobile drawer functional
    
    * Nav drawer animation
    
    * Update links
    
    * Nav anchor drawer fix
    
    * Stubbed placeholder pages
    
    * Homepage placeholder
    
    * Removed sandbox page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
-	import { useFloating, type Placement, autoUpdate, offset } from '$lib/index.js';
-
-	const elements = $state<{ reference?: HTMLElement; floating?: HTMLElement }>({});
-
-	let placement = $state<Placement>('bottom');
-
-	const { floatingStyles, x, y } = useFloating({
-		middleware: [offset(5)],
-		elements,
-		whileElementsMounted: autoUpdate
-	});
+	import IconArrowRight from 'lucide-svelte/icons/arrow-right';
+	import Logo from '$lib/components/Logo/Logo.svelte';
 </script>
 
-<select bind:value={placement}>
-	<option value="top">Top</option>
-	<option value="right">Right</option>
-	<option value="bottom">Bottom</option>
-	<option value="left">Left</option>
-</select>
-
-<p data-testid="x">{x.value}</p>
-<p data-testid="y">{y.value}</p>
-
-<button bind:this={elements.reference}>Reference</button>
-<div bind:this={elements.floating} style={floatingStyles.value}>Floating</div>
+<div class="space-y-10">
+	<header
+		class="h-[75vh] flex flex-col justify-center items-center space-y-8 text-center text-balance"
+	>
+		<Logo />
+		<h1 class="h1 text-black dark:text-white">Floating UI Svelte</h1>
+		<p>A Svelte library for position floating elements and create interactions for them.</p>
+		<div class="flex gap-4">
+			<a href="/tooltips" class="btn-cta">
+				<span>Get Started</span>
+				<IconArrowRight />
+			</a>
+			<a
+				href="https://github.com/skeletonlabs/floating-ui-svelte"
+				target="_blank"
+				class="btn-neutral"
+			>
+				GitHub
+			</a>
+		</div>
+	</header>
+</div>
---
+++ src/routes/tooltips/+page.svelte
@@ -0,0 +1,70 @@
+<div class="space-y-10">
+	<header class="card card-gradient space-y-8">
+		<h1 class="h1">
+			<span>Tooltips</span>
+		</h1>
+		<p>
+			A tooltip is a floating element that displays information related to an anchor element when it
+			receives keyboard focus or the mouse hovers over it.
+		</p>
+		<pre class="pre">{`import { useFloating } from '@skeletonlabs/floatin-ui-svelte';`}</pre>
+	</header>
+	<!-- Essentials -->
+	<section class="space-y-8">
+		<h2 class="h2">Essentials</h2>
+		<p>An accessible tooltip component has the following qualities:</p>
+		<ul class="list-disc list-outside translate-x-8 space-y-4">
+			<li>
+				<span class="highlight">Dynamic anchor positioning</span>: The tooltip is positioned next to
+				its reference element, and remains anchored to it while avoiding collisions.
+			</li>
+			<li>
+				<span class="highlight">Events</span>: When the mouse hovers over the reference element, or
+				when the reference element receives keyboard focus, the tooltip opens. When the mouse
+				leaves, or the reference is blurred, the tooltip closes.
+			</li>
+			<li>
+				<span class="highlight">Dismissal</span>: When the user presses the
+				<kbd class="kbd">esc</kbd> key while the tooltip is open, it closes.
+			</li>
+			<li>
+				<span class="highlight">Role</span>: The elements are given relevant role and ARIA
+				attributes to be accessible to screen readers.
+			</li>
+		</ul>
+	</section>
+	<section class="space-y-8">
+		<h2 class="h2">Examples</h2>
+		<p>Both of these examples have sections explaining them in-depth below.</p>
+		<div class="alert">Example Here</div>
+	</section>
+	<section class="space-y-8">
+		<h2 class="h2">Open state</h2>
+		<pre class="pre">{`
+import {useState} from 'react';\n
+function Tooltip() {
+	const [isOpen, setIsOpen] = useState(false);
+}`.trim()}</pre>
+		<p>
+			<code class="code">isOpen</code> determines whether or not the tooltip is currently open on the
+			screen. It is used for conditional rendering.
+		</p>
+	</section>
+	<!--
+	<p>
+		First, give the floating element initial CSS styles so that it becomes an
+		absolutely-positioned element that floats on top of the UI with layout ready for being
+		measured:
+	</p>
+	<p>
+		The <code class="code">-start</code> and <code class="code">-end</code> alignments are
+		<a href="/" class="anchor">logical</a> and will adapt to the writing direction (e.g. RTL) as expected.
+	</p>
+	<div class="alert">
+		<h3 class="h3">Note</h3>
+		<p>
+			You aren’t limited to just these 12 placements though. <code class="code">offset</code> allows
+			you to create any placement.
+		</p>
+	</div> -->
+</div>
