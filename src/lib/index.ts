42e03027d5de0e4485b592e556818b470562e1d7
Hugos68 <hugokorteapple@gmail.com>
2024-04-29 14:13:57 +0200

    Improve structure as prep for additional hooks

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
 export * from '@floating-ui/dom';
-export { useFloating, type UseFloatingReturn } from '$lib/hooks/use-floating.svelte.js';
+export { useFloating, type UseFloatingReturn } from '$lib/hooks/useFloating/index.svelte.js';
 export { type UseFloatingOptions } from '$lib/types.js';
