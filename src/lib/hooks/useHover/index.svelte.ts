52d8b2ef19a592a22154b63a8b12225b01f41c1b
Hugos68 <hugokorteapple@gmail.com>
2024-05-01 17:52:12 +0200

    Progress

--- src/lib/hooks/useHover/index.svelte.ts
+++ src/lib/hooks/useHover/index.svelte.ts
@@ -1,18 +1,38 @@
+import type { OpenChangeReason } from '$lib/types.js';
+import { createAttribute, isMouseLikePointerType, noop } from '$lib/utils.js';
 import type { Floating } from '../useFloating/index.svelte.js';
 import type { Interaction } from '../useInteractions/index.svelte.js';
+
+const safePolygonIdentifier = createAttribute('safe-polygon');
+
+export function getDelay(
+	value: HoverOptions['delay'],
+	prop: 'open' | 'close',
+	pointerType?: PointerEvent['pointerType']
+) {
+	if (pointerType && !isMouseLikePointerType(pointerType)) {
+		return 0;
+	}
+
+	if (typeof value === 'number') {
+		return value;
+	}
+
+	return value?.[prop];
+}
 
 interface DelayOptions {
 	/**
 	 * Time in ms that will delay the change of the open state to true.
 	 * @default 0
 	 */
-	show?: number;
+	open?: number;
 
 	/**
 	 * Time in ms that will delay the change of the open state to false.
 	 * @default 0
 	 */
-	hide?: number;
+	close?: number;
 }
 
 interface HandleCloseFn {
@@ -53,6 +73,9 @@ interface HoverOptions {
 	 */
 	move?: boolean;
 
+	/**
+	 * Callback to handle the closing of the floating element.
+	 */
 	handleClose?: HandleCloseFn | null;
 }
 
@@ -60,21 +83,8 @@ class Hover implements Interaction {
 	readonly #floating: Floating;
 	readonly #options: HoverOptions;
 	readonly #enabled = $derived.by(() => this.#options.enabled ?? true);
+	readonly #delay = $derived.by(() => this.#options.delay ?? 0);
 	readonly #mouseOnly = $derived.by(() => this.#options.mouseOnly ?? false);
-	readonly #openDelay = $derived.by(() => {
-		const delay = this.#options.delay;
-		if (typeof delay === 'number') {
-			return delay;
-		}
-		return delay?.show ?? 0;
-	});
-	readonly #closeDelay = $derived.by(() => {
-		const delay = this.#options.delay;
-		if (typeof delay === 'number') {
-			return delay;
-		}
-		return delay?.hide ?? 0;
-	});
 	readonly #restMs = $derived.by(() => this.#options.restMs ?? 0);
 	readonly #move = $derived.by(() => this.#options.move ?? true);
 	readonly #handleClose = $derived.by(() => this.#options.handleClose ?? null);
@@ -86,9 +96,41 @@ class Hover implements Interaction {
 		};
 	});
 
-	#timeout = -1;
+	#pointerType: string | undefined = undefined;
+	#handler: ((event: MouseEvent) => void) | undefined = undefined;
+	#timeout = 0;
 	#restTimeout = 1;
 	#blockMouse = true;
+	#performedPointerEventsMutation = false;
+	#unbindMouseMove = noop;
+
+	#clearPointerEvents() {
+		if (!this.#performedPointerEventsMutation) {
+			return;
+		}
+		document.body.style.pointerEvents = '';
+		document.body.removeAttribute(safePolygonIdentifier);
+		this.#performedPointerEventsMutation = false;
+	}
+
+	#closeWithDelay(event: Event, runElseBranch = true, reason: OpenChangeReason = 'hover') {
+		const closeDelay = getDelay(this.#delay, 'close', this.#pointerType);
+		if (closeDelay && !this.#handler) {
+			clearTimeout(this.#restTimeout);
+			this.#timeout = window.setTimeout(
+				() => this.#floating.onOpenChange(false, event, reason),
+				closeDelay
+			);
+		} else if (runElseBranch) {
+			clearTimeout(this.#timeout);
+			this.#floating.onOpenChange(false, event, reason);
+		}
+	}
+
+	#cleanupMouseMoveHandler() {
+		this.#unbindMouseMove();
+		this.#handler = undefined;
+	}
 
 	constructor(floating: Floating, options: HoverOptions = {}) {
 		this.#floating = floating;
@@ -133,7 +175,41 @@ class Hover implements Interaction {
 			};
 		});
 
-		// TODO: Continue porting from here. See: https://github.com/floating-ui/floating-ui/blob/master/packages/react/src/hooks/useHover.ts#L177
+		$effect(() => {
+			if (!this.#enabled) {
+				return;
+			}
+			const isClickLikeOpenEvent = () => {
+				if (!this.#floating.openEvent) {
+					return false;
+				}
+				return ['click', 'mousedown'].includes(this.#floating.openEvent.type);
+			};
+
+			const onMouseEnter = () => {
+				clearTimeout(this.#timeout);
+				this.#blockMouse = false;
+
+				if (
+					(this.#mouseOnly && !isMouseLikePointerType(this.#pointerType)) ||
+					(this.#restMs > 0 && !getDelay(this.#delay, 'open'))
+				) {
+					return;
+				}
+
+				const openDelay = getDelay(this.#delay, 'open', this.#pointerType);
+
+				if (openDelay) {
+					this.#timeout = window.setTimeout(() => {
+						this.#floating.onOpenChange(true, event, 'hover');
+					}, openDelay);
+				} else {
+					this.#floating.onOpenChange(true, event, 'hover');
+				}
+			};
+
+			// TODO: Continue from here - https://github.com/floating-ui/floating-ui/blob/master/packages/react/src/hooks/useHover.ts#L257
+		});
 	}
 
 	get reference() {
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -50,3 +50,20 @@ export function createPubSub() {
 		}
 	};
 }
+
+/**
+ * Checks if the given pointer type is mouse-like.
+ */
+export function isMouseLikePointerType(pointerType: string | undefined, strict?: boolean) {
+	// On some Linux machines with Chromium, mouse inputs return a `pointerType`
+	// of "pen": https://github.com/floating-ui/floating-ui/issues/2015
+	const values: Array<string | undefined> = ['mouse', 'pen'];
+	if (!strict) {
+		values.push('', undefined);
+	}
+	return values.includes(pointerType);
+}
+
+export function createAttribute(name: string) {
+	return `data-floating-ui-${name}`;
+}
