b1325609dfb48641fa86307d99ea7aaa1ee6307d
endigo9740 <gundamx9740@gmail.com>
2024-05-02 15:00:38 -0500

    homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,4 @@
 <script lang="ts">
-<<<<<<< HEAD
-	import { useFloating, type Placement, autoUpdate, offset } from '$lib/index.js';
-
-	const elements = $state<{ reference?: HTMLElement; floating?: HTMLElement }>({});
-	let placement = $state<Placement>('bottom');
-	const floating = useFloating({
-		get placement() {
-			return placement;
-		},
-		middleware: [offset(5)],
-		elements,
-		whileElementsMounted: autoUpdate
-	});
-</script>
-
-<select bind:value={placement}>
-	<option value="top">Top</option>
-	<option value="right">Right</option>
-	<option value="bottom">Bottom</option>
-	<option value="left">Left</option>
-</select>
-
-<p data-testid="x">{floating.x}</p>
-<p data-testid="y">{floating.y}</p>
-
-<button bind:this={elements.reference}>Reference</button>
-<div bind:this={elements.floating} style={floating.floatingStyles}>Floating</div>
-=======
 	import IconArrowRight from 'lucide-svelte/icons/arrow-right';
 	import Logo from '$docs/components/Logo/Logo.svelte';
 </script>
@@ -53,4 +25,3 @@
 		</div>
 	</header>
 </div>
->>>>>>> main
