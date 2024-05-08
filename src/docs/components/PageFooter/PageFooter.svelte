ecf222af7a238b6701ff3949f07deec9994da516
Chris Simmons <gundamx9740@gmail.com>
2024-05-08 16:20:16 -0500

    Move API reference from readme to documentation website. (#56)
    * New nav structure, nav component updates, and useFloating page started
    
    * Layout structure updated
    
    * Nav updates, error page added, install page updates
    
    * Linting fixes
    
    * Getting Started page ready
    
    * Table styles added, useFloating documented
    
    * Disable unused nav links
    
    * Favicon and doc refinement
    
    * Format and lint
    
    * Add docs table component
    
    * CodeBlock design updates
    
    * Stub navigation links and icons
    
    * Disable incomplete page links
    
    * Tweaks

--- src/docs/components/PageFooter/PageFooter.svelte
+++ src/docs/components/PageFooter/PageFooter.svelte
@@ -4,9 +4,13 @@
 
 <footer class="text-center space-y-2 p-4 py-10">
 	<p class="text-xs">
-		&copy; {currentYear}. MIT License. Maintained by
+		&copy; {currentYear}. MIT License. Inspired by
+		<a class="anchor" href="https://floating-ui.com/" target="_blank">Floating UI</a>.
+	</p>
+	<p class="text-xs">
+		By
 		<a href="https://github.com/Hugos68" target="_blank" class="anchor">Hugo Korte</a>,
 		<a href="https://www.skeletonlabs.co/" target="_blank" class="anchor">Skeleton Labs</a>, and the
-		<a href="https://svelte.dev/" target="_blank" class="anchor">Svelte Community</a>.
+		open source community.
 	</p>
 </footer>
---
+++ src/docs/components/Table/Table.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import type { TableData } from '$docs/types.js';
+	interface Props {
+		data: TableData[];
+	}
+	let { data }: Props = $props();
+</script>
+
+<div class="table-wrap">
+	<table class="table caption-bottom">
+		<tbody class="hover:[&>tr]:bg-surface-500/5">
+			{#each data as row}
+				<tr>
+					<!-- Property -->
+					<td>
+						<div class="flex items-center gap-2">
+							<code class="code">{row.property}</code>
+							<!-- Show red dot if required -->
+							{#if row.required}
+								<div class="bg-rose-500 aspect-square size-1.5 rounded-full"></div>
+							{/if}
+						</div>
+					</td>
+					<td class="space-y-1">
+						<!-- Description -->
+						<p>{row.description}</p>
+						<!-- Meta -->
+						<div class="flex items-center gap-2">
+							{#if row.type}<p class="meta-type" title="Type">{row.type}</p>{/if}
+							{#if row.default}<p class="meta-default" title="Default">{row.default}</p>{/if}
+						</div>
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
