fcfaaa5e90ae1575eee3e3e08da65e1f0376636e
hugos68 <hugokorteapple@gmail.com>
2024-05-09 23:56:56 +0200

    Added docs

---
+++ src/routes/(inner)/api/use-click/+page.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import CodeBlock from '$docs/components/CodeBlock/CodeBlock.svelte';
+	import Table from '$docs/components/Table/Table.svelte';
+	import { tableOptions } from './data.js';
+</script>
+
+<div class="space-y-10">
+	<!-- Header -->
+	<header class="card card-gradient space-y-8">
+		<h1 class="h1"><span>useClick</span></h1>
+		<p>Opens or closes the floating element when clicking the reference element.</p>
+		<CodeBlock lang="ts" code={`import { useClick } from '@skeletonlabs/floating-ui-svelte';`} />
+	</header>
+	<!-- Usage -->
+	<section class="space-y-8">
+		<h2 class="h2">Usage</h2>
+		<CodeBlock
+			lang="ts"
+			code={`
+import { useFloating, useInteractions, useClick } from '@skeletonlabs/floating-ui-svelte';
+
+const floating = useFloating();
+const click = useClick(floating.context);
+const interactions = useInteractions([hover]);
+`}
+		/>
+		<CodeBlock
+			lang="svelte"
+			code={`
+<!-- Reference Element -->
+<button
+	bind:this="{floating.elements.reference}"
+	{...interactions.getReferenceProps()}
+>
+	Reference
+</button>
+
+<!-- Floating Element -->
+<div
+	bind:this="{floating.elements.floating}"
+	style="{floating.floatingStyles}"
+	{...interactions.getFloatingProps()}
+>
+	Tooltip
+</div>
+		`}
+		/>
+	</section>
+	<!-- Table: Options -->
+	<section class="space-y-8">
+		<h2 class="h2">Options</h2>
+		<Table data={tableOptions} />
+	</section>
+	<!-- Compare -->
+	<section class="space-y-8">
+		<h2 class="h2">Compare</h2>
+		<!-- prettier-ignore -->
+		<p>
+			Compare to <a class="anchor" href="https://floating-ui.com/docs/useClick" target="_blank">Floating UI React</a>.
+		</p>
+	</section>
+</div>
---
+++ src/routes/(inner)/api/use-click/data.ts
@@ -0,0 +1,38 @@
+import type { TableData } from '$docs/types.js';
+
+// Options
+export const tableOptions: TableData[] = [
+	{
+		property: 'enabled',
+		description: 'Whether the Hook is enabled, including all internal Effects and event handlers.',
+		type: 'boolean',
+		default: 'true',
+	},
+	{
+		property: 'event',
+		description:
+			'The type of event to use to determine a "click" with mouse input. Keyboard clicks work as normal.',
+		type: '"click" | "mousedown"',
+		default: "'click'",
+	},
+	{
+		property: 'toggle',
+		description: 'Whether to toggle the open state with repeated clicks.',
+		type: 'boolean',
+		default: 'true',
+	},
+	{
+		property: 'ignoreMouse',
+		description:
+			'Whether to ignore the logic for mouse input (for example, if `useHover()` is also being used). When `useHover()` and `useClick()` are used together, clicking the reference element after hovering it will keep the floating element open even once the cursor leaves. This may not be desirable in some cases.',
+		type: 'boolean',
+		default: 'false',
+	},
+	{
+		property: 'keyboardHandlers',
+		description:
+			'Whether to add keyboard handlers (Enter and Space key functionality) for non-button elements (to open/close the floating element via keyboard "click").',
+		type: 'boolean',
+		default: 'true',
+	},
+];
