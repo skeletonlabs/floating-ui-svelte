0e3a6474c596200675691a7f43ae4fe18245347e
Jaakko Nakaza <jaakko.nakaza@aalto.fi>
2024-05-09 18:08:46 +0300

    Add FloatingArrow to the documentation website
    skeletonlabs/floating-ui-svelte#60

--- src/docs/components/Navigation/Navigation.svelte
+++ src/docs/components/Navigation/Navigation.svelte
@@ -14,7 +14,7 @@
 	// import IconUseHover from 'lucide-svelte/icons/square-mouse-pointer';
 	// import IconUseClick from 'lucide-svelte/icons/mouse-pointer-2';
 	// import IconUseRole from 'lucide-svelte/icons/person-standing';
-	// import IconFloatingArrow from 'lucide-svelte/icons/triangle';
+	import IconFloatingArrow from 'lucide-svelte/icons/triangle';
 	// import IconUtils from 'lucide-svelte/icons/wand-sparkles';
 	// Components
 	import Logo from '$docs/components/Logo/Logo.svelte';
@@ -45,7 +45,7 @@
 				// { icon: IconUseHover, href: '/api/use-hover', label: 'useHover' },
 				// { icon: IconUseClick, href: '/api/use-click', label: 'useClick' },
 				// { icon: IconUseRole, href: '/api/use-role', label: 'useRole' },
-				// { icon: IconFloatingArrow, href: '/api/floating-arrow', label: 'Floating Arrow' },
+				{ icon: IconFloatingArrow, href: '/api/floating-arrow', label: 'Floating Arrow' },
 				// { icon: IconUtils, href: '/api/utilities', label: 'Utilities' }
 			],
 		},
