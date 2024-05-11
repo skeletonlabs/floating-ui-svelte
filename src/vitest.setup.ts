b0eae6273a27c15739a587cc6d4dd4ca67ddbdb2
hugos68 <hugokorteapple@gmail.com>
2024-05-11 16:21:36 +0200

    Import order, comment

--- src/vitest.setup.ts
+++ src/vitest.setup.ts
@@ -5,10 +5,9 @@ import '@testing-library/jest-dom/vitest';
 
 /**
  * Injects a global afterEach to cleanup, see: https://testing-library.com/docs/svelte-testing-library/api#cleanup
- * TODO: Fix "import '@testing-library/svelte/vitest';" not working (it should be identical)
  */
-import { act, cleanup } from '@testing-library/svelte/svelte5';
 import { afterEach } from 'vitest';
+import { act, cleanup } from '@testing-library/svelte/svelte5';
 
 afterEach(async () => {
 	await act();
