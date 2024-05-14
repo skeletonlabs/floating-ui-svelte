3887d76244e29f89a53aaaa2eb5d2c3c03cf7a1b
hugos68 <hugokorteapple@gmail.com>
2024-05-15 00:40:49 +0200

    Added missing tests for useRole

--- src/lib/hooks/useRole/index.test.svelte.ts
+++ src/lib/hooks/useRole/index.test.svelte.ts
@@ -88,6 +88,133 @@ describe('useRole', () => {
 			});
 		});
 
-		// Continue here: https://github.com/floating-ui/floating-ui/blob/master/packages/react/test/unit/useRole.test.tsx#L163
+		describe('menu', () => {
+			it('applies the `menu` role to the floating element', () => {
+				render(App, { role: 'menu', open: true });
+
+				expect(screen.getByTestId('floating')).toHaveAttribute('role', 'menu');
+			});
+
+			it('applies the `aira-haspopup` attribute to the reference element', () => {
+				render(App, { role: 'menu' });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute('aria-haspopup', 'menu');
+			});
+
+			it('applies the `aria-expanded` attribute to the reference element based on the `open` state', async () => {
+				const { rerender } = render(App, { role: 'menu', open: false });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute('aria-expanded', 'false');
+
+				await rerender({ open: true });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute('aria-expanded', 'true');
+			});
+
+			it('applies the `aria-controls` attribute with the correct id to the reference element based on the `open` state', async () => {
+				const { rerender } = render(App, { role: 'menu', open: false });
+
+				expect(screen.getByTestId('reference')).not.toHaveAttribute('aria-controls');
+
+				await rerender({ open: true });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute(
+					'aria-controls',
+					screen.getByTestId('floating').id,
+				);
+			});
+		});
+
+		describe('listbox', () => {
+			it('applies the `listbox` role to the floating element', () => {
+				render(App, { role: 'listbox', open: true });
+
+				expect(screen.getByTestId('floating')).toHaveAttribute('role', 'listbox');
+			});
+
+			it('applies the `aria-haspopup` attribute to the reference element', () => {
+				render(App, { role: 'listbox' });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute('aria-haspopup', 'listbox');
+			});
+
+			it('applies the `aria-expanded` attribute to the reference element based on the `open` state', async () => {
+				const { rerender } = render(App, { role: 'listbox', open: false });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute('aria-expanded', 'false');
+
+				await rerender({ open: true });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute('aria-expanded', 'true');
+			});
+
+			it('applies the `aria-controls` attribute with the correct id to the reference element based on the `open` state', async () => {
+				const { rerender } = render(App, { role: 'listbox', open: false });
+
+				expect(screen.getByTestId('reference')).not.toHaveAttribute('aria-controls');
+
+				await rerender({ open: true });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute(
+					'aria-controls',
+					screen.getByTestId('floating').id,
+				);
+			});
+		});
+
+		describe('select', () => {
+			it('applies the `listbox` role to the floating element', () => {
+				render(App, { role: 'select', open: true });
+
+				expect(screen.getByTestId('floating')).toHaveAttribute('role', 'listbox');
+			});
+
+			it('applies the `aria-autocomplete` attribute to the reference element', () => {
+				render(App, { role: 'select' });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute('aria-autocomplete', 'none');
+			});
+
+			it('applies the `aria-expanded` attribute to the reference element based on the `open` state', async () => {
+				const { rerender } = render(App, { role: 'select', open: false });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute('aria-expanded', 'false');
+
+				await rerender({ open: true });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute('aria-expanded', 'true');
+			});
+
+			it('applies the `aria-selected` attribute to the selected item within the floating element', async () => {
+				render(App, { role: 'select', open: true });
+
+				expect(screen.getByTestId('item-1')).toHaveAttribute('aria-selected', 'false');
+				expect(screen.getByTestId('item-2')).toHaveAttribute('aria-selected', 'true');
+			});
+		});
+
+		describe('combobox', () => {
+			it('applies the `listbox` role to the floating element', () => {
+				render(App, { role: 'combobox', open: true });
+
+				expect(screen.getByTestId('floating')).toHaveAttribute('role', 'listbox');
+			});
+
+			it('applies the `aria-autocomplete` attribute to the reference element', () => {
+				render(App, { role: 'combobox' });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute('aria-autocomplete', 'list');
+			});
+
+			it('applies the `aria-expanded` attribute to the reference element based on the `open` state', async () => {
+				const { rerender } = render(App, { role: 'combobox', open: false });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute('aria-expanded', 'false');
+
+				await rerender({ open: true });
+
+				expect(screen.getByTestId('reference')).toHaveAttribute('aria-expanded', 'true');
+			});
+		});
 	});
 });
