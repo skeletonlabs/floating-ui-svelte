4b839e74bc4206c75c39f8ea515b1b3e728901eb
hugos68 <hugokorteapple@gmail.com>
2024-05-27 16:03:38 +0200

    revert

--- src/lib/hooks/useFocus/index.svelte.ts
+++ src/lib/hooks/useFocus/index.svelte.ts
@@ -117,22 +117,18 @@ function useFocus(context: FloatingContext, options: UseFocusOptions = {}) {
 					const target = getTarget(event);
 
 					if (visibleOnly && isElement(target)) {
-						if (!target.matches(':focus-visible')) {
-							return;
+						try {
+							// Mac Safari unreliably matches `:focus-visible` on the reference
+							// if focus was outside the page initially - use the fallback
+							// instead.
+							if (isSafari() && isMac()) throw Error();
+							if (!target.matches(':focus-visible')) return;
+						} catch {
+							// Old browsers will throw an error when using `:focus-visible`.
+							if (!keyboardModality && !isTypeableElement(target)) {
+								return;
+							}
 						}
-
-						// try {
-						// 	// Mac Safari unreliably matches `:focus-visible` on the reference
-						// 	// if focus was outside the page initially - use the fallback
-						// 	// instead.
-						// 	if (isSafari() && isMac()) throw Error();
-						// 	if (!target.matches(':focus-visible')) return;
-						// } catch {
-						// 	// Old browsers will throw an error when using `:focus-visible`.
-						// 	if (!keyboardModality && !isTypeableElement(target)) {
-						// 		return;
-						// 	}
-						// }
 					}
 
 					onOpenChange(true, event, 'focus');
