1cabb5e7cf68fc3730cbce0682dcc04b15aff329
hugos68 <hugokorteapple@gmail.com>
2024-05-27 15:59:24 +0200

    test

--- src/lib/hooks/useFocus/index.test.ts
+++ src/lib/hooks/useFocus/index.test.ts
@@ -1,32 +1,30 @@
 import { describe, expect, it, vi } from 'vitest';
-import { render, screen } from '@testing-library/svelte';
+import { render, screen, fireEvent } from '@testing-library/svelte';
 import { userEvent } from '@testing-library/user-event';
 import App from './App.test.svelte';
 
 describe('useFocus', () => {
 	describe('default', () => {
 		it('changes the open state to `true` on focus', async () => {
-			const user = userEvent.setup();
 			render(App);
 
 			expect(screen.queryByTestId('floating')).not.toBeInTheDocument();
 
-			await user.tab();
+			await fireEvent.focus(screen.getByTestId('reference'));
 
 			expect(screen.getByTestId('floating')).toBeInTheDocument();
 		});
 
 		it('changes the open state to `false` on blur', async () => {
-			const user = userEvent.setup();
 			render(App);
 
 			expect(screen.queryByTestId('floating')).not.toBeInTheDocument();
 
-			await user.tab();
+			await fireEvent.focus(screen.getByTestId('reference'));
 
 			expect(screen.getByTestId('floating')).toBeInTheDocument();
 
-			await user.tab();
+			await fireEvent.blur(screen.getByTestId('reference'));
 
 			await vi.waitFor(() => {
 				expect(screen.queryByTestId('floating')).not.toBeInTheDocument();
@@ -36,23 +34,21 @@ describe('useFocus', () => {
 
 	describe('enabled', () => {
 		it('does enable the hook when set to `true`', async () => {
-			const user = userEvent.setup();
 			render(App, { enabled: true });
 
 			expect(screen.queryByTestId('floating')).not.toBeInTheDocument();
 
-			await user.tab();
+			await fireEvent.focus(screen.getByTestId('reference'));
 
 			expect(screen.getByTestId('floating')).toBeInTheDocument();
 		});
 
 		it('does not enable the hook when set to `false`', async () => {
-			const user = userEvent.setup();
 			render(App, { enabled: false });
 
 			expect(screen.queryByTestId('floating')).not.toBeInTheDocument();
 
-			await user.tab();
+			await fireEvent.focus(screen.getByTestId('reference'));
 
 			await vi.waitFor(() => {
 				expect(screen.queryByTestId('floating')).not.toBeInTheDocument();
@@ -62,45 +58,41 @@ describe('useFocus', () => {
 
 	describe('visibleOnly', () => {
 		it('does change the open state to `true` on focus when set to `true`', async () => {
-			const user = userEvent.setup();
 			render(App, { visibleOnly: true });
 
 			expect(screen.queryByTestId('floating')).not.toBeInTheDocument();
 
-			await user.tab();
+			await fireEvent.focus(screen.getByTestId('reference'));
 
 			expect(screen.getByTestId('floating')).toBeInTheDocument();
 		});
 
 		it('does not change the open state to `true` on click (focus, but not focus-within) when set to `true`', async () => {
-			const user = userEvent.setup();
 			render(App, { visibleOnly: true });
 
 			expect(screen.queryByTestId('floating')).not.toBeInTheDocument();
 
-			await user.click(screen.getByTestId('reference'));
+			await userEvent.click(screen.getByTestId('reference'));
 
 			expect(screen.queryByTestId('floating')).not.toBeInTheDocument();
 		});
 
 		it('does change the open state to `true` on focus when set to `false`', async () => {
-			const user = userEvent.setup();
 			render(App, { visibleOnly: false });
 
 			expect(screen.queryByTestId('floating')).not.toBeInTheDocument();
 
-			await user.click(screen.getByTestId('reference'));
+			await fireEvent.focus(screen.getByTestId('reference'));
 
 			expect(screen.queryByTestId('floating')).toBeInTheDocument();
 		});
 
 		it('does change the open state to `true` on click (focus, but not focus-within) when set to `false`', async () => {
-			const user = userEvent.setup();
 			render(App, { visibleOnly: false });
 
 			expect(screen.queryByTestId('floating')).not.toBeInTheDocument();
 
-			await user.click(screen.getByTestId('reference'));
+			await userEvent.click(screen.getByTestId('reference'));
 
 			expect(screen.queryByTestId('floating')).toBeInTheDocument();
 		});
