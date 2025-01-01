0bba933bf0931e99b022e5bbaab64a646c59be46
Hugos68 <hugokorteapple@gmail.com>
2025-01-01 17:10:05 +0100

    Fixed build issue

--- packages/floating-ui-svelte/svelte.config.js
+++ packages/floating-ui-svelte/svelte.config.js
@@ -1,12 +1,9 @@
-import adapter from "@sveltejs/adapter-static";
+// @ts-check
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter(),
-	},
+	preprocess: [vitePreprocess()],
 };
 
 export default config;
